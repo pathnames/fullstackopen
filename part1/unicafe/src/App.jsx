30bbf9c7f6dc4d9b8b5b25b16de077c4428f29b6
pathnames <pk58@buffalo.edu>
2025-01-25 12:58:43 -0500

    Exercise 1.7 & 1.8 solution

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -12,6 +12,19 @@ const Button = ({ onClick, text }) => (
   </button>
 )
 
+const Statistics = ({good, neutral, bad}) => {
+  return (
+    <div> 
+      <h1>Statistics</h1>
+      <h3>good {good}</h3>
+      <h3>neutral {neutral}</h3>
+      <h3>bad {bad}</h3>
+      <h3> all {good + neutral + bad}</h3>
+      <h3> average {(good + neutral + bad) / 3}</h3>
+      <h3> positive {(good / (good + neutral + bad)) * 100}%</h3>
+    </div>
+  )
+}
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
@@ -25,11 +38,10 @@ const App = () => {
   }
   const decreaseByOne = () => {
     console.log("decreasing, value before: ", good)
-    setGood(good - 1)
+    setBad(bad + 1)
   }
   const doNothing = () => {
     console.log("doing nothing, value before: ", good)
-    setGood(good)
   }
 
   return (
@@ -38,6 +50,7 @@ const App = () => {
       <Button onClick={increaseByOne} text='good' />
       <Button onClick={doNothing} text='neutral' />
       <Button onClick={decreaseByOne} text='bad' />
+      <Statistics good={good} bad={bad} neutral={neutral}/>
     </div>
   )
 }
