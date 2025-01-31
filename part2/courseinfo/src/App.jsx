a2f41b6401f51c4e7d5ef9f144f15eca9d1a0b56
Prathamesh Kulkarni <pk58@buffalo.edu>
2025-01-31 10:37:33 -0500

    Exercise 2.4 & 2.5 solution.

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -1,41 +1,4 @@
-const Header = (props) => {
-  return (<h1> {props.course.name} </h1>)
-}
-
-const Content = ({ course }) => {
-  return (
-    <>
-      {course.parts.map(({ id, name, exercises }) => (
-        <Part key={id} name={name} exercises={exercises} />
-      ))}
-    </>
-  );
-};
-
-const Part = ({id, name, exercises}) => {
-return (
-  <>
-  <p>{name} {exercises}</p>
-  </>
-)
-} 
-
-const Total = ({ parts }) => {
-  return (
-    <>
-      <p>Number of exercises {parts.reduce((acc, part) => acc + part.exercises, 0)}</p>
-    </>
-  )
-}
-const Course = ({ course }) => {
-  return (
-    <div>
-      <Header course={course} />
-      <Content course={course} />
-      <Total parts={course.parts} />
-    </div>
-  )
-}
+import Course from '../components/Course'
 
 const App = () => {
   const course = {
