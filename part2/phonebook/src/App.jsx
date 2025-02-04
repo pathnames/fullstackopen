4945855ff1dee06fc6c49487af87f4bc78d21987
pathnames <pk58@buffalo.edu>
2025-02-04 10:29:36 -0500

    Exercise 2.7 solution.

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -10,7 +10,10 @@ const App = () => {
     const nameObject = {
       name: newName,
     }
-  
+    if (persons.find(person => person.name === newName)) {
+      alert(`${newName} is already added to phonebook`)
+      return
+    }
     setPersons(persons.concat(nameObject))
     setNewName('')
   }
