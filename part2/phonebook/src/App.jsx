aa14735d0136237ea6acc3c0e8e8d3acd817cbee
pathnames <pk58@buffalo.edu>
2025-02-05 16:54:00 -0500

    Exercise 2.9 solution.

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,10 +1,15 @@
 import { useState } from 'react'
 
 const App = () => {
-  const [persons, setPersons] = useState([]) 
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+  ]) 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
-
+  const [searchTerm, setNewSearchTerm] = useState('')
   const addFormData = (event) => {
     event.preventDefault()
     const nameObject = {
@@ -26,10 +31,14 @@ const App = () => {
   const handleNumberChange = (event) => {
     setNewNumber(event.target.value)
   }
-
+  const handleSearchTermChange = (event) => {
+    setNewSearchTerm(event.target.value)
+    setPersons(persons.filter(person => person.name.includes(searchTerm)))
+  }
   return (
     <div>
       <h2>Phonebook</h2>
+      filter shown with<input value={searchTerm} onChange={handleSearchTermChange}/>
       <form onSubmit={addFormData}>
         <div>
           name: <input value={newName} onChange={handleNameChange}/>
