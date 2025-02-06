89f6da8bbaa1bab9022fee37a610b2de5da7461b
pathnames <pk58@buffalo.edu>
2025-02-05 20:48:09 -0500

    Exercise 2.10 solution.

---
+++ part2/phonebook/components/Filter.jsx
@@ -0,0 +1,8 @@
+const Filter = ({ searchTerm, handleSearchTermChange }) => {
+    return (
+        <div>
+            filter shown with <input value={searchTerm} onChange={handleSearchTermChange} />
+        </div>
+    )
+}
+export default Filter
--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,4 +1,7 @@
 import { useState } from 'react'
+import PersonForm from '../components/PersonForm'
+import Persons from '../components/Persons'
+import Filter from '../components/Filter'
 
 const App = () => {
   const [persons, setPersons] = useState([
@@ -6,10 +9,11 @@ const App = () => {
     { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
     { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
     { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ]) 
+  ])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
-  const [searchTerm, setNewSearchTerm] = useState('')
+  const [searchTerm, setSearchTerm] = useState('')
+
   const addFormData = (event) => {
     event.preventDefault()
     const nameObject = {
@@ -25,36 +29,19 @@ const App = () => {
     setNewNumber('')
   }
 
-  const handleNameChange = (event) => {
-    setNewName(event.target.value)
-  }
-  const handleNumberChange = (event) => {
-    setNewNumber(event.target.value)
-  }
+
   const handleSearchTermChange = (event) => {
-    setNewSearchTerm(event.target.value)
+    setSearchTerm(event.target.value)
     setPersons(persons.filter(person => person.name.includes(searchTerm)))
   }
+
   return (
     <div>
       <h2>Phonebook</h2>
-      filter shown with<input value={searchTerm} onChange={handleSearchTermChange}/>
-      <form onSubmit={addFormData}>
-        <div>
-          name: <input value={newName} onChange={handleNameChange}/>
-        </div>
-        <div>number: <input value={newNumber} onChange={handleNumberChange}/>
-        </div>
-        <div>
-          <button type="submit">add</button>
-        </div>
-      </form>
+      <Filter searchTerm={searchTerm} handleSearchTermChange={handleSearchTermChange} />
+      <PersonForm addFormData={addFormData} newName={newName} handleNameChange={(event) => setNewName(event.target.value)} newNumber={newNumber} handleNumberChange={(event) => setNewNumber(event.target.value)} />
       <h2>Numbers</h2>
-      <ul>
-       {persons.map(person => 
-        <li key={person.name}>{person.name} {person.number}</li>
-       )}
-      </ul>
+      <Persons persons={persons} />
     </div>
   )
 }
