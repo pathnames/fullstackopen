2adac0085eb8e15bef63f9aaf26fc200d034e40c
pathnames <pk58@buffalo.edu>
2025-02-08 18:36:31 -0500

    Exercise 2.15 solution.

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -26,7 +26,15 @@ const App = () => {
       number: newNumber,
     }
     if (persons.find(person => person.name === newName)) {
-      alert(`${newName} is already added to phonebook`)
+      if (window.confirm(`${newName} is already added to phonebook, replace the old number with a new one?`)) {
+        const person = persons.find(person => person.name === newName)
+        const changedPerson = { ...person, number: newNumber }
+        phoneService
+          .update(person.id, changedPerson)
+          .then(response => {
+            setPersons(persons.map(person => person.id !== changedPerson.id ? person : changedPerson))
+          })
+      }
       return
     }
     phoneService
