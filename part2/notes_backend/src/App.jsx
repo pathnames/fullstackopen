18370532d474b0bca34ce6635926c27c84a0299e
pathnames <pk58@buffalo.edu>
2025-02-08 18:54:37 -0500

    CSS Lessons for notes_backend.

--- part2/notes_backend/src/App.jsx
+++ part2/notes_backend/src/App.jsx
@@ -2,11 +2,38 @@ import { useState, useEffect } from 'react'
 import Note from '../components/Note'
 import axios from 'axios'
 import noteService from './services/notes'
+import './App.css'
+import './index.css'
 
+const Footer = () => {    
+  const footerStyle = { 
+    color: 'green',
+    fontStyle: 'italic',
+    fontSize: 16
+  }
+  return (  
+    <div style={footerStyle}>
+      <br />
+      <em>Note app, Department of Computer Science, University of Helsinki 2021</em>
+    </div>
+  )
+}
+const Notification = ({ message }) => {
+  if (message === null) {
+    return null
+  }
+
+  return (
+    <div className='error'>
+      {message}
+    </div>
+  )
+}
 const App = (props) => {
   const [notes, setNotes] = useState([])
   const [newNote, setNewNote] = useState('')
   const [showAll, setShowAll] = useState(false)
+  const [errorMessage, setErrorMessage] = useState('some error happened...')
 
   useEffect(() => {
     console.log('effect')
@@ -18,6 +45,18 @@ const App = (props) => {
   }, [])
   console.log('render', notes.length, 'notes')
 
+  const Notification = ({ message }) => {
+    if (message === null) {
+      return null
+    }
+  
+    return (
+      <div className='error'>
+        {message}
+      </div>
+    )
+  }
+
   const toggleImportanceOf = id => {
     const note = notes.find(n => n.id === id)
     const changedNote = { ...note, important: !note.important }
@@ -28,9 +67,12 @@ const App = (props) => {
       })
   
       .catch(error => {
-        alert(
-          `the note '${note.content}' was already deleted from server`
+        setErrorMessage(
+          `Note '${note.content}' was already removed from server`
         )
+        setTimeout(() => {        
+          setErrorMessage(null)
+        }, 5000)
         setNotes(notes.filter(n => n.id !== id))
       })
   }
@@ -61,6 +103,7 @@ const App = (props) => {
   return (
     <div>
       <h1>Notes</h1>
+      <Notification message={errorMessage} />
       <div>
         <button onClick={() => setShowAll(!showAll)}>
           show {showAll ? 'important' : 'all' }
