b2c2fa0dfee26976c9cf695c1c470ccdc5145942
Prathamesh Kulkarni <pk58@buffalo.edu>
2025-02-12 10:03:33 -0500

    Serving static files from the backend

--- notes_backend/index.js
+++ notes_backend/index.js
@@ -3,6 +3,7 @@ const cors = require('cors')
 const app = express()
 app.use(express.json())
 app.use(cors())
+app.use(express.static('dist'))
 
 let notes = [{ id: "1", content: "HTML is easy", important: true }, { id: "2", content: "Browser can execute only JavaScript", important: false }, { id: "3", content: "GET and POST are the most important methods of HTTP protocol", important: true }]
 
