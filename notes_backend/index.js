bb2283951b93edcff82341c9630a68bc6da4f592
Prathamesh Kulkarni <pk58@buffalo.edu>
2025-02-14 12:30:17 -0500

    Committing before exercise 3.15

--- notes_backend/index.js
+++ notes_backend/index.js
@@ -16,22 +16,48 @@ app.get('/api/notes', (req, res) => {
     Note.find({}).then(notes => {
         res.json(notes)
     })
+    .catch(error => {
+        res.status(500).send({ error: 'something went wrong' })
+    })
 })
 
-app.get('/api/notes/:id', (req, res) => {
+app.get('/api/notes/:id', (req, res, next) => {
     const id = req.params.id
-    const note = notes.find(note => note.id === id)
-    if (note) {
-        res.json(note)
-    } else {
-        res.status(404).end()
-    }
+    Note.findById(id)
+        .then(note => {
+            if (note) {
+                res.json(note)
+            } else {
+                res.status(404).end()
+            }
+        })
+        .catch(error => {
+            next(error)          
+        })
 })
 
 app.delete('/api/notes/:id', (req, res) => {
     const id = req.params.id
-    notes = notes.filter(note => note.id !== id)
-    res.status(204).end()
+    Note.findByIdAndDelete(id)
+        .then(result => {
+            res.status(204).end()
+        })
+        .catch(error => next(error))
+})
+
+app.put('/api/notes/:id', (request, response, next) => {
+    const body = request.body
+  
+    const note = {
+      content: body.content,
+      important: body.important,
+    }
+  
+    Note.findByIdAndUpdate(request.params.id, note, { new: true })
+      .then(updatedNote => {
+        response.json(updatedNote)
+      })
+      .catch(error => next(error))
 })
 
 app.post('/api/notes', (req, res) => {
@@ -48,6 +74,16 @@ app.post('/api/notes', (req, res) => {
         res.json(savedNote)
     })
 })
+
+const errorHandler = (error, req, res, next) => {
+    console.error(error.message)
+    if (error.name === 'CastError') {
+        return res.status(400).send({ error: 'malformatted id' })
+    }
+    next(error)
+}
+
+app.use(errorHandler)
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
