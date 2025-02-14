0a3b8036d8b4c9fb89a713f17951b88be7e12e61
Prathamesh Kulkarni <pk58@buffalo.edu>
2025-02-14 10:55:47 -0500

    Changing frontend for exercise 3.11

--- part2/phonebook/components/Persons.jsx
+++ part2/phonebook/components/Persons.jsx
@@ -2,7 +2,7 @@ const Persons = ({ persons, handleDelete}) => {
   return (
     <ul>
       {persons.map(person => 
-      <li key={person.id}>
+      <li key={person._id}>
       {person.name} {person.number} 
       <button onClick={() => handleDelete(person.id)}> delete</button>
       </li>
